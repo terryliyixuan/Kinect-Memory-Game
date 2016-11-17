5a5b11abb9d90e45d622621568d8759ffb0c3e8e
Terry Li <terryliyixuan@gmail.com>
2016-11-17 01:50:16 -0500

    Scripts
    1. Updated "Shape" script; 2. Added a "ShapeManager" script

--- Assets/Resources/Scripts/Shape.cs
+++ Assets/Resources/Scripts/Shape.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Shape : MonoBehaviour {
+public class Shape : MonoBehaviour
+{
+
+	// Data related to this game object
+	public string myName;
+	public string myShape;
+	public Color myColor;
+
+	// Declare components
+	private Renderer myRenderer;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		// Set the name of this game object
+		gameObject.name = myName;
+
+		// Set the color of this game object
+		myRenderer = GetComponent<Renderer> ();
+		myRenderer.material.color = myColor;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 }
---
+++ Assets/Resources/Scripts/ShapeManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeManager : MonoBehaviour
+{
+
+	private Shape[] shapesInScene;
+
+	// Use this for initialization
+	void Start ()
+	{
+		shapesInScene = GameObject.FindObjectsOfType<Shape> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
