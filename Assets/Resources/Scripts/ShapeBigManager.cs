32c78aaa6d9b98aac788008b5eae362c5b49a4b5
Terry Li <terryliyixuan@gmail.com>
2016-12-04 16:16:08 -0500

    Editing
    Terry - 1. Edited the ShapeBigManager.cs. Now shapes could be
    instantiated with random colors; 2. Added a temporary shape prefab.

--- Assets/Resources/Scripts/ShapeBigManager.cs
+++ Assets/Resources/Scripts/ShapeBigManager.cs
@@ -8,9 +8,13 @@ public class ShapeBigManager : MonoBehaviour
 	// Public variables
 	// ---------------------------
 	public int minNum = 1;
-	public int maxNum = 2;
+	public int maxNum = 5;
+	public float cdTime = 2f;
 	public GameObject[] shapePrefabs;
 	public Color[] presetColors;
+
+
+
 	public static int totalShapeCounter = 0;
 	public static int totalColorCounter = 0;
 
@@ -18,28 +22,50 @@ public class ShapeBigManager : MonoBehaviour
 	// Private variables
 	// ---------------------------
 	private GameObject shapeInstantiated;
+	private GameObject shapeParent;
+	private float cdTimer;
+	private int shapeAmount;
+	private int shapeAmountCounter = 0;
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+		shapeAmount = (int)Random.Range (minNum, maxNum);
+		shapeParent = GameObject.Find ("Shapes");
+		SetCDTimer ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		cdTimer -= Time.deltaTime;
+			
+		if (shapeAmountCounter < shapeAmount && cdTimer < 0) {
+			int rand = Random.Range (0, shapePrefabs.Length);
+			Vector3 pos = new Vector3 (Random.Range (0, 10), Random.Range (0, 10), Random.Range (0, 10));
+			InstantiateShape (rand, pos);
+			AssignColor ();
+			shapeAmountCounter++;
+			SetCDTimer ();
+		}
 	}
 
+	// Instantiate a shape
 	void InstantiateShape (int _num, Vector3 _pos)
 	{
 		shapeInstantiated = Instantiate (shapePrefabs [_num], _pos, Quaternion.identity) as GameObject;
-		shapeInstantiated.transform.parent = GameObject.Find ("Shapes").transform;
+		shapeInstantiated.transform.parent = shapeParent.transform;
 	}
 
+	// Assign a color to the instantiated shape
 	void AssignColor ()
 	{
-		int rand = (int)Random.Range (0, presetColors.Length - 1);
+		int rand = (int)Random.Range (0, presetColors.Length);
+		shapeInstantiated.GetComponent<Renderer> ().material.color = presetColors [rand];
+	}
 
+	void SetCDTimer ()
+	{
+		cdTimer = cdTime;
 	}
 }
