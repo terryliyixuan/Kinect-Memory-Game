01f0b1b22b042a7ae655a91c3489904dd1b8a4b0
Terry Li <terryliyixuan@gmail.com>
2017-02-07 21:22:47 -0500

    Changes
    1. Made an “Intro Scene”; 2. Added a “TouchToSwitchScene.cs” script to
    jump to the next scene; 3. Made the temporary player a prefab.

---
+++ Assets/Resources/Scripts/TouchToSwitchScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Use this script to switch the scene by touching the ball
+
+public class TouchToSwitchScene : MonoBehaviour
+{
+
+	// ---------------------------
+	// Public variables
+	// ---------------------------
+	// Declare the name of the next scene
+	public string nextSceneName;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		SceneManager.LoadScene (nextSceneName);
+	}
+		
+}
