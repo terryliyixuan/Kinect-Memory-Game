7cd402e0b6362a111205962d8bb3bb015708d7cd
Terry Li <terryliyixuan@gmail.com>
2017-05-14 21:52:35 -0400

    Changes
    1. Made an “End Circle” prefab; 2. Edited the “GameManager.cs”; 3.
    Adjusted the DogeScene.unity - Added a score canvas

--- Assets/Resources_Dodge/Scripts/GameManager.cs
+++ Assets/Resources_Dodge/Scripts/GameManager.cs
@@ -16,6 +16,11 @@ public class GameManager : MonoBehaviour
 	public int startCountDownTime;
 	[Header ("Ending Variables")]
 	public string endText;
+	public Camera scoreCamera;
+	public Text scoreText;
+	public float endCDTime;
+	//public GameObject scoreBody;
+
 
 	public static bool isPlayerDead = false;
 	public static bool hasGameStarted;
@@ -37,14 +42,19 @@ public class GameManager : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		// Before game starts
+		// Display title text
 		if (hasGameStarted == false) {
 			if (startTextTime > 0) {
 				timerText.text = startText;
 				timerText.fontSize = 100;
 				startTextTime -= Time.deltaTime;
-			} else {
+			}
+			// Before game starts
+			// Display count down text
+			else {
 				if ((Time.timeSinceLevelLoad - 3) >= startCountDownTimer && startCountDownTimer <= startCountDownTime) {
-					Debug.Log ("counting down cd time");
+					timerText.fontSize = 250;
 					startNumber -= 1;
 					timerText.text = startNumber.ToString ();
 					startCountDownTimer += 1;
@@ -55,12 +65,28 @@ public class GameManager : MonoBehaviour
 					hasGameStarted = true;
 				}
 			}
-		} else {
+		}
+
+		// Game starts
+		// Display time
+		else {
 			if (isPlayerDead == false) {
 				currentTime += Time.deltaTime;
 				timerText.text = currentTime.ToString ("f0");
-			} else {
+				scoreText.text = timerText.text;
+			} 
+			// Game ends
+			// Display ending text
+			else {
 				timerText.text = endText;
+				if (endCDTime > 0) {
+					endCDTime -= Time.deltaTime;
+				} else {
+					Camera.main.gameObject.SetActive (false);
+					scoreCamera.gameObject.SetActive (true);
+					//scoreBody.gameObject.SetActive (true);
+					Destroy (gameObject);
+				}
 			}
 		}
 	}
