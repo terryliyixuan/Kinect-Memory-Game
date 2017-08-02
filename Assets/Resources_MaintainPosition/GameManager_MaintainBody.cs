7ed35172b0bfa63619822c05657b2eae7c5d7de7
Terry Li <terryliyixuan@gmail.com>
2017-08-02 17:31:45 -0400

    Changes
    Added new folders for Maintain-Position game

---
+++ Assets/Resources_MaintainPosition/GameManager_MaintainBody.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_MaintainBody : MonoBehaviour
+{
+
+	public GameObject[] targets;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	private void RemoveAt<T> (ref T[] arr, int index)
+	{
+		for (int a = index; a < arr.Length - 1; a++) {
+			arr [a] = arr [a + 1];
+		}
+
+	}
+}
